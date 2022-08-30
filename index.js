9829ca5e549aa24f916baec78440a1ac3c34fdb5
zt <98013370+nkssnwn@users.noreply.github.com>
2022-08-30 06:34:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: ["allIntents"] });
+const Monitor = require('ping-monitor');
+const keepAlive = require('./server');
+const config = require("./config.json")
+
+keepAlive();
+const monitor = new Monitor({
+  website: 'https://OfficialBotNSKSKDKDK.vitrolkoyoma.repl.co',
+  title: 'Secundario',
+  interval: 15 // minutes
+});
+
+monitor.on('up', (res) => console.log(`${res.website} está encedido.`));
+monitor.on('down', (res) => console.log(`${res.website} se ha caído - ${res.statusMessage}`));
+monitor.on('stop', (website) => console.log(`${website} se ha parado.`));
+monitor.on('error', (error) => console.log(error));
+
+client.on('ready', () => {
+  console.log(`Ready in  ${client.user.tag}!`); 
+
+  client.user.setStatus('available')
+    client.user.setPresence({
+        game: {
+            name: '',
+            type: ""
+        }
+    });
+});
+
+client.on("message", message => {
+  if(message.content === ".test") {
+    message.channel.send("Toy Joya")
+  }
+})
+
+
+
+
+client.login(process.env['TOKEN']);
